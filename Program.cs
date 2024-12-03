8c44470c2d9659f75e959c1c24dcbfddbb8271d6
Zangetsu001 <159882846+Zangetsu001@users.noreply.github.com>
2024-12-03 14:23:23 +0200

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    ///
+    class Program
+    {
+        /// <summary>
+        /// Выполнить заполнение одномерного массива
+        /// </summary>
+        /// <param name="args"></param>
+        static void Main(string[] args)
+        {
+            int size = GetSize();
+            int[] array = Input(size);
+            Output(array);
+            Console.ReadKey();
+        }
+        static int GetSize()
+        {
+            Console.Write("Введите имя массива: ");
+            int size;
+            while (!int.TryParse(Console.ReadLine(), out size) || size <= 0)
+            {
+                Console.WriteLine("Ошибка, введите еще раз! ");
+            }
+            return size;
+        }
+        static int[] Input(int size)
+        {
+            int[] array = new int[size];
+            for(int i = 0; i < size; i++)
+            {
+                Console.Write($"array{i} = ");
+                array[i] = int.Parse(Console.ReadLine());
+            }
+            return array;
+        }
+        static void Output(int[] array)
+        {
+            for(int i = 0; i < array.Length; i++)
+            {
+                Console.WriteLine($"array[{i}] = {array[i]}");
+            }
+        }
+
+    }
+}
